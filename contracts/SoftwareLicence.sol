69648a2a5fe76a3c1dcfb8fd4f6d57d54bdb5806
yanesh <yaneshcode@gmail.com>
2019-02-09 05:00:55 -0500

    working contract

--- contracts/SoftwareLicence.sol
+++ contracts/SoftwareLicence.sol
@@ -2,11 +2,11 @@ pragma solidity ^0.5.0;
 
 contract SoftwareLicence {
 
-    // Events
-    event TransferOwner(address from, address to);
-    event AddToWhitelist(address recipient);
-    event RemoveFromWhitelist(address recipient);
-    event RedeemLicence(address recipient, uint tokenId, uint timestamp);
+  // Events
+  event TransferOwner(address from, address to);
+  event AddToWhitelist(address recipient);
+  event RemoveFromWhitelist(address recipient);
+  event RedeemLicence(address recipient, uint tokenId, uint timestamp);
 
   // Variables
   string public name = "Software Licence Token";
@@ -22,62 +22,68 @@ contract SoftwareLicence {
   Token[] public tokens;
 
   mapping (address => uint) public ownerToTokenId;
-
   mapping (address => bool) public whitelist;
 
+  // Constructor
   constructor() public {
     owner = msg.sender;
 
-    Token memory token = Token({
+    tokens.push(
+      Token({
         redeemedBy: address(0x0),
         redeemedAt: uint(now)
-      });
-
-      // mintevent
-      tokens.push(token) - 1;
+    });
   }
 
+  // owner modifier
   modifier onlyOwner() {
     require(msg.sender == owner);
     _;
   }
 
+  // whitelist modifier
   modifier onlyWhitelist() {
     require(whitelist[msg.sender]);
     _;
   }
 
+  // transfer ownership
   function transferOwner(address newOwner) public onlyOwner
   {
     emit TransferOwner(owner, newOwner);
     owner = newOwner;
   }
 
+  // add to whitelist
   function addWhitelist(address _addr) public onlyOwner {
+    emit AddToWhitelist(_addr);
     whitelist[_addr] = true;
   }
 
+  // remove from whitelist
   function removeWhitelist(address _addr) public onlyOwner {
+    emit RemoveFromWhitelist(_addr);
     whitelist[_addr] = false;
   }
 
+  // user redeem licence
   function redeem() public onlyWhitelist returns (uint tokenId){
 
     require(ownerToTokenId[msg.sender] == 0);
 
-    Token memory token = Token({
-        redeemedBy: msg.sender,
-        redeemedAt: uint(now)
-      });
+    uint memory timestamp = now;
 
-      // mintevent
+    Token memory token = Token({
+      redeemedBy: msg.sender,
+      redeemedAt: timestamp
+    });
+
     tokenId = tokens.push(token) - 1;
 
     ownerToTokenId[msg.sender] = tokenId;
-    /* ownershipTokenCount[msg.sender]++; */
-    /* tokenIndexToOwner[tokenId] = msg.sender; */
 
-    // event
+    emit RedeemLicence(msg.sender, tokenId, timestamp);
+
   }
 
 }
