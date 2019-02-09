3d78776574020e2bbdec8c511f0fca06bc84d8db
yanesh <yaneshcode@gmail.com>
2019-02-09 00:46:05 -0500

    nft using openzeppelin

--- contracts/MerkleAirdropNFT.sol
+++ contracts/MerkleAirdropNFT.sol
@@ -1,5 +1,26 @@
 pragma solidity ^0.5.0;
 
+import 'openzeppelin-solidity/contracts/token/ERC721/ERC721Full.sol';
+contract NFT is ERC721Full {
+  constructor() ERC721Full("Collectible", "COL") public {}
+
+  struct Colour {
+    uint8 red;
+  }
+
+  Colour[] colours;
+
+  function mint() public {
+    Colour memory _colour = Colour(uint8(now));
+    uint _id = colours.push(_colour) - 1;
+    _mint(msg.sender, _id);
+  }
+
+  function getColourFromId(uint id) public view returns(uint8) {
+    return (colours[id].red);
+  }
+}
+
 contract MerkleAirdropNFT {
   address public owner;
 
